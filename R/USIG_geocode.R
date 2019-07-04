f42cfb336a2ededc7f388f3f685ed99562820888
HAVB <avazquez@gmail.com>
2019-07-04 18:00:56 -0300

    Adding mutate_USIG_geocode

--- R/USIG_geocode.R
+++ R/USIG_geocode.R
@@ -2,7 +2,6 @@
 #' Geocoder basado en el servicio de USIG GCABA (http://usig.buenosaires.gob.ar/)
 #' uso: USIG_geocode(direccion)
 #'
-#' @import futile.logger
 #' @import httr
 #' @param direccion Una dirección dentro de los límites de la Región Urbana Metropolitana de Buenos Aires
 #' @examples
@@ -12,58 +11,75 @@
 #'
 #' resultado
 #'
-#'               address              address_normalised        lat        lng
-#' 9 de julio y belgrano 9 DE JULIO AV. y BELGRANO, CABA -34.613090 -58.381226
+#'             address_normalised        lon        lat
+#'9 DE JULIO AV. y BELGRANO, CABA -58.381226 -34.613090
+#'
 #'
 #' Si se trata de una dirección fuera de la Ciudad Autónoma de Buenos Aires, explicitar el municipio o partido
 #' USIG_geocode("9 de julio y belgrano, temperley")
 #'
 #'  resultado
 #'
-#'                address              address_normalised        lat        lng
-#' 9 de julio y belgrano 9 DE JULIO AV. y BELGRANO, CABA -34.613090 -58.381226
+#'                                                   address_normalised       lon       lat
+#'Avenida 9 de Julio y Paso bajo nivel Manuel Belgrano, Lomas de Zamora -58.39645 -34.77974
+#'
+#' Se pueden georeferenciar varias direcciones a la vez:
+#'
+#' USIG_geocode(c("9 de Julio y Belgrano, Temperley", "Callao y Corrientes", "Anchorena 1210, La Lucila"))
+#'
+#'  resultado
+#'                                                    address_normalised               lon               lat
+#' Avenida 9 de Julio y Paso bajo nivel Manuel Belgrano, Lomas de Zamora       -58.3964491       -34.7797373
+#'                                     CALLAO AV. y CORRIENTES AV., CABA        -58.392293        -34.604434
+# '                                  Tomás Anchorena 1210, Vicente López -58.4935336530612 -34.5009281857143
 
 USIG_geocode <- function(address) {
 
-    base_url <- "http://servicios.usig.buenosaires.gob.ar/normalizar/"
+    make_call <- function(address) {
 
-    make_address_query <- function(address) {
-        query <- paste0(base_url,
-                        "?direccion=",
-                        address,
-                        "&geocodificar=TRUE")
-    }
+        #stopifnot(is.character(address))
 
-    query <- URLencode(make_address_query(address))
+        base_url <- "http://servicios.usig.buenosaires.gob.ar/normalizar/"
 
-    flog.info(paste("Trying", query))
-
-    results <- tryCatch(httr::GET(query),
-                        error = function(error_message) {return(NULL)})
-
-
-
-    if (!httr:::is.response(results)) {
-        data.frame(address = address, address_normalised = NA,
-                   lat = NA, lng = NA, stringsAsFactors = FALSE)
-    } else {
-        results <- httr::content(results)
-        if (length(results$direccionesNormalizadas)) {
-            x <- results$direccionesNormalizadas[[1]]$coordenadas$x
-            y <- results$direccionesNormalizadas[[1]]$coordenadas$y
-            address_normalised <- results$direccionesNormalizadas[[1]]$direccion
-        } else {
-            x <- NA
-            y <- NA
-            address_normalised <- NA
+        make_address_query <- function(address) {
+            query <- paste0(base_url,
+                            "?direccion=",
+                            address,
+                            "&geocodificar=TRUE")
         }
 
+        query <- URLencode(make_address_query(address))
 
-        data.frame(address = address,
-                   address_normalised = ifelse(is.null(address_normalised), NA, address_normalised),
-                   lat = ifelse(is.null(y), NA, y),
-                   lng = ifelse(is.null(x), NA, x),
-                   stringsAsFactors = FALSE)
+        paste("Trying", query)
+
+        results <- tryCatch(httr::GET(query),
+                            error = function(error_message) {return(NULL)})
+
+
+
+        if (!httr:::is.response(results)) {
+            data.frame(address = address, address_normalised = NA,
+                       lon = NA, lat = NA, stringsAsFactors = FALSE)
+        } else {
+            results <- httr::content(results)
+            if (length(results$direccionesNormalizadas)) {
+                x <- results$direccionesNormalizadas[[1]]$coordenadas$x
+                y <- results$direccionesNormalizadas[[1]]$coordenadas$y
+                address_normalised <- results$direccionesNormalizadas[[1]]$direccion
+            } else {
+                x <- NA
+                y <- NA
+                address_normalised <- NA
+            }
+
+
+            data.frame(address_normalised = ifelse(is.null(address_normalised), NA, address_normalised),
+                       lon = ifelse(is.null(x), NA, x),
+                       lat = ifelse(is.null(y), NA, y),
+                       stringsAsFactors = FALSE)
+        }
     }
+
+    Reduce(rbind, lapply(address, make_call))
 
 }
